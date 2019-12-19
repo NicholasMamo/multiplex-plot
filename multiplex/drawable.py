f6e42d2ab9927c7849d41303d2c90bc7a19760c9
NicholasMamo <nicholasmamo@gmail.com>
2019-12-19 15:55:44 +0100

    Create the initial Drawable class

--- multiplex/drawable.py
+++ multiplex/drawable.py
@@ -1,4 +1,30 @@
 """
-A class that wraps the figure and provide more functionality to work with visualizations.
+A class that wraps a figure and provides more functionality to work with visualizations.
 """
 
+import matplotlib.pyplot as plt
+
+class Drawable():
+	"""
+	The :class:`Drawable` class wraps a matplotlib figure and axis to provide additional functionality.
+
+	:ivar figure: The figure that the :class:`Drawable` class wraps.
+	:vartype figure: :class:`matplotlib.figure.Figure`
+	:ivar axis: The axis where the drawable will draw.
+	:vartype axis: :class:`matplotlib.axis.Axis`
+	"""
+
+	def __init__(self, figure, axis=None):
+		"""
+		Create the drawable with the figure.
+
+		:param figure: The figure that the :class:`Drawable` class wraps.
+					   This is mainly used to get the figure renderer.
+		:type figure: :class:`matplotlib.figure.Figure`
+		:param axis: The axis (or subplot) where to plot visualizations.
+					 If `None` is not given, the plot's main subplot is used instead.
+		:type axis: `None` or :class:`matplotlib.axis.Axis`
+		"""
+
+		self.figure = figure
+		self.axis = plt.gca() if axis is None else axis
