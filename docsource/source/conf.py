dcae7ff99340447a923f8e43c15b600ccc1073e4
NicholasMamo <nicholasmamo@gmail.com>
2019-12-24 15:50:59 +0100

    Update the documentation's version information

--- docsource/source/conf.py
+++ docsource/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Nicholas Mamo'
 author = 'Nicholas Mamo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
