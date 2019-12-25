b73fc7cd0dd6e8e357cfdf189c80a76ce2982143
NicholasMamo <nicholasmamo@gmail.com>
2019-12-25 13:22:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multiplex-plot", # Replace with your own username
+    version="0.1.0",
+    author="Nicholas Mamo",
+    author_email="nicholasmamo@gmail.com",
+    description="Multiplex: visualizations that tell stories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NicholasMamo/multiplex-plot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Visualization",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3',
+)
