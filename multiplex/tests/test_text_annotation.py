4495c4f37e023dfce82d01028f776e0a5d7f1613
NicholasMamo <nicholasmamo@gmail.com>
2019-12-26 16:09:35 +0100

    Add alignment tests to the annotation class

--- multiplex/tests/test_text_annotation.py
+++ multiplex/tests/test_text_annotation.py
@@ -66,6 +66,59 @@ class TestTextAnnotation(unittest.TestCase):
 
 			y = bb.y1
 
+	def test_align_left(self):
+		"""
+		Test that when aligning text left, all lines start at the same x-coordinate.
+		"""
+
+		text = 'Memphis Depay, commonly known simply as Memphis, is a Dutch professional footballer and music artist who plays as a forward and captains French club Lyon and plays for the Netherlands national team. He is known for his pace, ability to cut inside, dribbling, distance shooting and ability to play the ball off the ground.'
+		viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
+		lines = viz.draw_text_annotation(text.split(), align='left')
+
+		x = 0
+		for i, (_, tokens) in enumerate(lines):
+			bb = util.get_bb(viz.figure, viz.axis, tokens[0])
+			if i == 0:
+				x = bb.x0
+
+			self.assertLessEqual(x, bb.x0)
+
+	def test_align_right(self):
+		"""
+		Test that when aligning text right, all lines end at the same x-coordinate.
+		"""
+
+		text = 'Memphis Depay, commonly known simply as Memphis, is a Dutch professional footballer and music artist who plays as a forward and captains French club Lyon and plays for the Netherlands national team. He is known for his pace, ability to cut inside, dribbling, distance shooting and ability to play the ball off the ground.'
+		viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
+		lines = viz.draw_text_annotation(text.split(), align='right')
+
+		x = 0
+		for i, (_, tokens) in enumerate(lines):
+			bb = util.get_bb(viz.figure, viz.axis, tokens[-1])
+			if i == 0:
+				x = bb.x1
+
+			self.assertLessEqual(x, bb.x1)
+
+	def test_align_justify(self):
+		"""
+		Test that when justifying text, all lines start and end at the same x-coordinate.
+		"""
+
+		text = 'Memphis Depay, commonly known simply as Memphis, is a Dutch professional footballer and music artist who plays as a forward and captains French club Lyon and plays for the Netherlands national team. He is known for his pace, ability to cut inside, dribbling, distance shooting and ability to play the ball off the ground.'
+		viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
+		lines = viz.draw_text_annotation(text.split(), align='justify')
+
+		x0, x1 = 0, 0
+		for i, (_, tokens) in enumerate(lines[:-1]):
+			bb = util.get_bb(viz.figure, viz.axis, tokens[-1])
+			if i == 0:
+				x0 = bb.x0
+				x1 = bb.x1
+
+			self.assertLessEqual(x0, bb.x0)
+			self.assertLessEqual(x1, bb.x1)
+
 	def _reconstruct_text(self, lines):
 		"""
 		Reconstruct the visualization text from a list of lines.
