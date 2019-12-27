f6e3e69fc27c97f5666968cea70c255c61816b68
NicholasMamo <nicholasmamo@gmail.com>
2019-12-27 16:20:15 +0100

    Create the initial time series class

---
+++ multiplex/timeseries/timeseries.py
@@ -0,0 +1,18 @@
+"""
+The :class:`timeseries.timeseries.TimeSeries` class borrows heavily on matplotlib's `plot` function.
+This class builds on matplotlib's plotting and introduces more functionality.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+import util
+
+class TimeSeries(object):
+	"""
+	The :class:`timeseries.timeseries.TimeSeries` class borrows heavily on matplotlib's `plot` function.
+	This class builds on matplotlib's plotting and introduces more functionality.
+	"""
+
+	pass
