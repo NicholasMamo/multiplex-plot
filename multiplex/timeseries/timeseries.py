87f46495ccd35e68c2be76adb5c95170cad8958b
NicholasMamo <nicholasmamo@gmail.com>
2019-12-27 16:33:06 +0100

    Add the constructor and draw function to the TimeSeries

--- multiplex/timeseries/timeseries.py
+++ multiplex/timeseries/timeseries.py
@@ -1,6 +1,9 @@
 """
 The :class:`timeseries.timeseries.TimeSeries` class borrows heavily on matplotlib's `plot` function.
 This class builds on matplotlib's plotting and introduces more functionality.
+
+To start creating time series visualizations, create a :class:`timeseries.timeseries.TimeSeries` instance and call the :meth:``timeseries.timeseries.TimeSeries.draw` method.
+If you are using the :class:`drawable.Drawable` class, just call the :meth:`drawable.Drawable.draw_time_series` method on a :class:`drawable.Drawable` instance instead.
 """
 
 import os
@@ -13,6 +16,29 @@ class TimeSeries(object):
 	"""
 	The :class:`timeseries.timeseries.TimeSeries` class borrows heavily on matplotlib's `plot` function.
 	This class builds on matplotlib's plotting and introduces more functionality.
+
+	:ivar drawable: The :class:`drawable.Drawable` where the time series visualization will be drawn.
+	:vartype drawable: :class:`drawable.Drawable`
 	"""
 
-	pass
+	def __init__(self, drawable):
+		"""
+		Initialize the text annotation with the figure and axis.
+		The figure is used to get the renderer.
+		The visualization is drawn on the given axis.
+
+		:param drawable: The :class:`drawable.Drawable` where the time series visualization will be drawn.
+		:type drawable: :class:`drawable.Drawable`
+		"""
+
+		self.drawable = drawable
+
+	def draw(self, *args, **kwargs):
+		"""
+		Draw a time series on the :class:`drawable.Drawable`.
+		The arguments and keyword arguments are passed on to the :meth:`matplotlib.pyplot.plot` method.
+		Thus, all of the arguments and keyword arguments accepted by it are also accepted by this function.
+		"""
+
+		axis = self.drawable.axis
+		axis.plot(*args, **kwargs)
