0256908af2fff7f007aca2b653c55147818cbf9f
NicholasMamo <nicholasmamo@gmail.com>
2019-12-29 13:25:19 +0100

    Update the time series documentation

--- multiplex/timeseries/timeseries.py
+++ multiplex/timeseries/timeseries.py
@@ -2,6 +2,9 @@
 The :class:`timeseries.timeseries.TimeSeries` class borrows heavily on matplotlib's `plot` function.
 This class builds on matplotlib's plotting and introduces more functionality.
 
+For example, Multiplex time series do not have a legend by default.
+Instead, to aid readability, the line's label is added to the end of the plot.
+
 To start creating time series visualizations, create a :class:`timeseries.timeseries.TimeSeries` instance and call the :meth:`timeseries.timeseries.TimeSeries.draw` method.
 If you are using the :class:`drawable.Drawable` class, just call the :meth:`drawable.Drawable.draw_time_series` method on a :class:`drawable.Drawable` instance instead.
 """
