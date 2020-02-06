b15e29f4e50630ac41a4d0747ecb26025309c1cc
NicholasMamo <nicholasmamo@gmail.com>
2020-02-06 17:41:20 +0100

    Add the annotation test to the test script

--- tests.sh
+++ tests.sh
@@ -6,6 +6,7 @@ cd "$parent_path" # go to the script path
 
 # Perform the unit tests
 
+python3 -m unittest multiplex.tests.test_annotation
 python3 -m unittest multiplex.tests.test_drawable
 python3 -m unittest multiplex.tests.test_text
 python3 -m unittest multiplex.tests.test_time_series
