529492323897b8e00da418a9fff6f552809a2ec0
NicholasMamo <nicholasmamo@gmail.com>
2020-02-06 19:07:11 +0100

    Create version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multiplex-plot", # Replace with your own username
-    version="0.1.5",
+    version="0.2.0",
     author="Nicholas Mamo",
     author_email="nicholasmamo@gmail.com",
     description="Multiplex: visualizations that tell stories",
