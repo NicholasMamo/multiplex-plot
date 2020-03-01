33eb310518bab2d501fae9279fa198075f5898ad
NicholasMamo <nicholasmamo@gmail.com>
2020-03-01 11:24:54 +0100

    Create the initial labelled visualization class

---
+++ multiplex/labelled.py
@@ -0,0 +1,14 @@
+"""
+A labelled visualization is one that allows labels.
+These labels are normal :class:`~text.annotation.TextAnnotation`.
+However, a labelled visualization has functionality to ensure that the labels do not overlap.
+"""
+
+class LabelledVisualization(object):
+	"""
+	The labelled visualization adds functionality to visualizations that use labels.
+	Labels are normal :class:`~text.annotation.TextAnnotation`.
+	This class adds functionality to distribute overlapping labels.
+	"""
+
+	pass
