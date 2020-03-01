b904faf6927ec3870398dfeec08f4736d77f1ee1
NicholasMamo <nicholasmamo@gmail.com>
2020-03-01 15:27:30 +0100

    Create a new visualization class to store the drawable

---
+++ multiplex/visualization.py
@@ -0,0 +1,21 @@
+"""
+A simple visualization is made up of :class:`~drawable.Drawable`.
+"""
+
+class Visualization(object):
+	"""
+	All visualizations contain at least a :class:`~drawable.Drawable`.
+
+	:ivar drawable: The :class:`~drawable.Drawable` where the time series visualization will be drawn.
+	:vartype drawable: :class:`~drawable.Drawable`
+	"""
+
+	def __init__(self, drawable, *args, **kwargs):
+		"""
+		Create the visualization with a drawable.
+
+		:param drawable: The :class:`~drawable.Drawable` where the time series visualization will be drawn.
+		:type drawable: :class:`~drawable.Drawable`
+		"""
+
+		self.drawable = drawable
