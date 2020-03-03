51a29f7934ec2fd9e3c54444a6a6b5dfda8641ec
NicholasMamo <nicholasmamo@gmail.com>
2020-03-03 17:57:51 +0100

    Update the library version of the documentation

--- docsource/conf.py
+++ docsource/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Nicholas Mamo'
 author = 'Nicholas Mamo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.5'
 
 
 # -- General configuration ---------------------------------------------------
