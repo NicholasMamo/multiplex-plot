e7e1d5b2e136f8a61f58367200369e84fffc1e7c
NicholasMamo <nicholasmamo@gmail.com>
2020-03-04 12:21:28 +0100

    Create a new generic test class

---
+++ multiplex/tests/test.py
@@ -0,0 +1,29 @@
+"""
+General functions for Multiplex unit tests.
+"""
+
+import matplotlib.pyplot as plt
+import unittest
+
+class MultiplexTest(unittest.TestCase):
+	"""
+	General functions for Multiplex unit tests.
+	"""
+
+	@staticmethod
+	def temporary_plot(f):
+		"""
+		The temporary plot decorator function removes the plot after every test.
+		In this way, the memory of the plot is freed.
+		"""
+
+		def wrapper(*args, **kwargs):
+			"""
+			Call the test function with any arguments and keyword arguments.
+			Immediately after, close the plot to free the memory.
+			"""
+
+			f(*args, **kwargs)
+			plt.close()
+
+		return wrapper
