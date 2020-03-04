c166190e2b0a20d11419dbad63110afbea715ed8
NicholasMamo <nicholasmamo@gmail.com>
2020-03-04 12:07:56 +0100

    Update the drawable tests to use the annotation representation directly

--- multiplex/tests/test_drawable.py
+++ multiplex/tests/test_drawable.py
@@ -26,9 +26,8 @@ class TestDrawable(unittest.TestCase):
 		text = 'caption.'
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		caption_tokens = viz.set_caption(text)
-		caption_text = self._reconstruct_caption(caption_tokens)
-		self.assertEqual(text, caption_text)
+		caption = viz.set_caption(text)
+		self.assertEqual(text, str(caption))
 
 	def test_caption_removes_multiple_spaces(self):
 		"""
@@ -40,9 +39,8 @@ class TestDrawable(unittest.TestCase):
 		"""
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		caption_tokens = viz.set_caption(text)
-		caption_text = self._reconstruct_caption(caption_tokens)
-		self.assertEqual('This is a multi-level caption.', caption_text)
+		caption = viz.set_caption(text)
+		self.assertEqual('This is a multi-level caption.', str(caption))
 
 	def test_caption_removes_tabs(self):
 		"""
@@ -54,36 +52,5 @@ class TestDrawable(unittest.TestCase):
 		"""
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		caption_tokens = viz.set_caption(text)
-		caption_text = self._reconstruct_caption(caption_tokens)
-		self.assertEqual('This is a multi-level caption.', caption_text)
-
-	def test_caption_retains_newlines(self):
-		"""
-		Test that the caption preprocessing does not remove newlines.
-		"""
-
-		text = """
-			This is a multi-level
-			caption.
-		"""
-
-		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		caption_tokens = viz.set_caption(text)
-		caption_text = self._reconstruct_caption(caption_tokens)
-		self.assertEqual('This is a multi-level\ncaption.', caption_text)
-
-	def _reconstruct_caption(self, caption_tokens):
-		"""
-		Reconstruct the caption text from a list of lines.
-		The method expects nested lists.
-		Each high-level list is a line, made up of another list of tokens.
-
-		:param caption_tokens: A list of lists, each list a line of tokens.
-		:type caption_tokens: list of list
-
-		:return: The re-constructed text.
-		:rtype: str
-		"""
-
-		return '\n'.join([ ' '.join([ token.get_text() for token in caption_line ]) for caption_line in caption_tokens ])
+		caption = viz.set_caption(text)
+		self.assertEqual('This is a multi-level caption.', str(caption))
