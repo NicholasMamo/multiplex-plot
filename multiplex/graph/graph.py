e76c00aa0251786963f3086bc588e96f90b5ac5f
NicholasMamo <nicholasmamo@gmail.com>
2020-04-29 21:02:47 +0200

    Create functions to draw graph nodes and edges

--- multiplex/graph/graph.py
+++ multiplex/graph/graph.py
@@ -1,8 +1,14 @@
 """
 The :class:`~Graph` class can be used to draw directed or undirected graphs.
 Each graph is made up of nodes and edges and can be used to show the relations between nodes.
+
+.. note::
+
+	The graph visualization uses the `networkx` package to generate the layout of the graph.
+	Therefore it is a prerequisite to create these visualizations.
 """
 
+import networkx as nx
 import os
 import sys
 
@@ -38,3 +44,60 @@ class Graph(LabelledVisualization):
 		:rtype: tuple
 		"""
 
+		positions = nx.spring_layout(G)
+		nodes = self._draw_nodes(positions, s=s, *args, **kwargs)
+		edges = self._draw_edges(G.edges, positions, *args, **kwargs)
+		return nodes, edges
+
+	def _draw_nodes(self, positions, s, *args, **kwargs):
+		"""
+		Draw the nodes onto the :class:`~drawable.Drawable`.
+
+		Any additional arguments and keyword arguments are passed on to the :func:`matplotlib.pyplot.scatter` function.
+
+		The nodes should be given as dictionaries, whose keys are the node names.
+		The corresponding values are their positions.
+
+		:param positions: The positions of the nodes as a dictionary.
+						  The keys are the node names, and the values are the corresponding positions.
+		:type positions: dict
+		:param s: The size of the nodes.
+		:type s: float
+
+		:return: The list of drawn nodes.
+		:rtype: :class:`matplotlib.collections.PathCollection`
+		"""
+
+		nodes = [ ]
+
+		"""
+		Extract the node positions and draw scatter plots.
+		"""
+		x = [ position[0] for position in positions.values() ]
+		y = [ position[1] for position in positions.values() ]
+		nodes = self.drawable.scatter(x, y, s=s, *args, **kwargs)
+
+		return nodes
+
+	def _draw_edges(self, edges, nodes, *args, **kwargs):
+		"""
+		Draw the edges connecting the given nodes.
+
+		Any additional arguments and keyword arguments are passed on to the :func:`matplotlib.pyplot.plot` function.
+
+		:param edges: The list of edges to draw.
+					  The edges should be a list of tuples representing the source and target.
+		:type edges: list of tuple
+		:param nodes: The nodes in terms of their positions as a dictionary.
+					  The keys are the node names, and the values are the corresponding positions.
+		:type nodes: dict
+		"""
+
+		paths = [ ]
+
+		for edge in edges:
+			source, target = nodes[edge[0]], nodes[edge[1]]
+			x, y = (source[0], target[0]), (source[1], target[1])
+			paths.append(self.drawable.plot(x, y, zorder=-1, *args, **kwargs))
+
+		return paths
--- multiplex/tests/test_graph.py
+++ multiplex/tests/test_graph.py
@@ -33,3 +33,73 @@ class TestGraph(MultiplexTest):
 		self.assertFalse(len(nodes.get_offsets()))
 		self.assertFalse(edges)
 
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_single_node(self):
+		"""
+		Test drawing a graph with one node.
+		"""
+
+		G = nx.Graph()
+		G.add_node(1)
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, edges = viz.draw_graph(G)
+		self.assertEqual(1, len(nodes.get_offsets()))
+		self.assertFalse(edges)
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_without_edges(self):
+		"""
+		Test drawing a graph with multiple nodes, but no edges.
+		"""
+
+		G = nx.Graph()
+		G.add_node(1)
+		G.add_node(2)
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, edges = viz.draw_graph(G)
+		self.assertEqual(2, len(nodes.get_offsets()))
+		self.assertFalse(edges)
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_with_single_edge(self):
+		"""
+		Test that when drawing a graph with one edge, the correct nodes are connected.
+		"""
+
+		G = nx.Graph()
+		G.add_node(1)
+		G.add_node(2)
+		G.add_node(3)
+		G.add_edge(1, 3)
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, edges = viz.draw_graph(G)
+		self.assertEqual(3, len(nodes.get_offsets()))
+		self.assertEqual(1, len(edges))
+		self.assertEqual(nodes.get_offsets()[0][0], edges[0][0].get_xdata()[0])
+		self.assertEqual(nodes.get_offsets()[0][1], edges[0][0].get_ydata()[0])
+		self.assertEqual(nodes.get_offsets()[2][0], edges[0][0].get_xdata()[1])
+		self.assertEqual(nodes.get_offsets()[2][1], edges[0][0].get_ydata()[1])
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_with_multiple_edges(self):
+		"""
+		Test that when drawing a graph with one edge, the correct nodes are connected.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A') ]
+		G = nx.from_edgelist(E)
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, edges = viz.draw_graph(G)
+		self.assertEqual(3, len(nodes.get_offsets()))
+		self.assertEqual(2, len(edges))
+		indices = [ (0, 1), (0, 2) ]
+		for i, edge in enumerate(edges):
+			source, target = indices[i]
+			self.assertEqual(nodes.get_offsets()[source][0], edge[0].get_xdata()[0])
+			self.assertEqual(nodes.get_offsets()[source][1], edge[0].get_ydata()[0])
+			self.assertEqual(nodes.get_offsets()[target][0], edge[0].get_xdata()[1])
+			self.assertEqual(nodes.get_offsets()[target][1], edge[0].get_ydata()[1])
