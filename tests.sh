9aaf619c0643997ddd3f9db36e734dd6194f0fa4
NicholasMamo <nicholasmamo@gmail.com>
2020-04-29 21:03:12 +0200

    Add the graph unit tests to the general test script

--- tests.sh
+++ tests.sh
@@ -10,5 +10,6 @@ python3 -m unittest multiplex.tests.test_annotation
 python3 -m unittest multiplex.tests.test_drawable
 python3 -m unittest multiplex.tests.test_legend
 python3 -m unittest multiplex.tests.test_labelled
+python3 -m unittest multiplex.tests.test_graph
 python3 -m unittest multiplex.tests.test_text
 python3 -m unittest multiplex.tests.test_time_series
