5b3a6be448b6af3bdd4e830a81b3d200a6e37d32
NicholasMamo <nicholasmamo@gmail.com>
2020-05-02 18:15:15 +0200

    Update the graph to allow drawing labels next to nodes

--- multiplex/graph/graph.py
+++ multiplex/graph/graph.py
@@ -29,7 +29,7 @@ class Graph(LabelledVisualization):
 
 		super().__init__(*args, **kwargs)
 
-	def draw(self, G, node_style=None, edge_style=None, *args, **kwargs):
+	def draw(self, G, node_style=None, name_style=None, edge_style=None, *args, **kwargs):
 		"""
 		Draw the given graph.
 
@@ -44,24 +44,31 @@ class Graph(LabelledVisualization):
 						   They are passed on as keyword arguments to the :func:`~graph.graph.Graph._draw_nodes` function.
 						   Individual nodes can override this style using the `style` attribute.
 		:type node_style: dict
+		:param name_style: The general style for names.
+						   Keys correspond to the styling parameter and the values are the styling value.
+						   They are passed on as keyword arguments to the :func:`~graph.graph.Graph._draw_node_names` function.
+						   Individual names can override this style using the `style` attribute.
+		:type name_style: dict
 		:param edge_style: The general style for edges.
 						   Keys correspond to the styling parameter and the values are the styling value.
 						   They are passed on as keyword arguments to the :func:`~graph.graph.Graph._draw_edges` function.
 						   Individual edges can override this style using the `style` attribute.
 		:type edge_style: dict
 
-		:return: A tuple containing the list of drawn nodes and edges.
+		:return: A tuple containing the list of drawn nodes, their names, and edges.
 		:rtype: tuple
 		"""
 
 		node_style = node_style or { }
+		name_style = name_style or { }
 		edge_style = edge_style or { }
 
 		self.drawable.axis.axis('off')
 		positions = nx.spring_layout(G, *args, **kwargs)
 		nodes = self._draw_nodes(G.node, positions, **node_style)
+		node_names = self._draw_node_names(G.node, positions, **name_style)
 		edges = self._draw_edges(G.edges, positions, **edge_style)
-		return nodes, edges
+		return nodes, node_names, edges
 
 	def _draw_nodes(self, nodes, positions, *args, **kwargs):
 		"""
@@ -73,7 +80,7 @@ class Graph(LabelledVisualization):
 		The corresponding values are their positions.
 
 		:param nodes: The list of actual nodes to draw.
-		:type nodes: list of
+		:type nodes: networkx.classes.reportviews.NodeView
 		:param positions: The positions of the nodes as a dictionary.
 						  The keys are the node names, and the values are the corresponding positions.
 		:type positions: dict
@@ -95,6 +102,70 @@ class Graph(LabelledVisualization):
 			rendered.append(self.drawable.scatter(x, y, *args, **node_style))
 
 		return rendered
+
+	def _draw_node_names(self, nodes, positions, *args, **kwargs):
+		"""
+		Draw labels for the nodes.
+		Labels are drawn if they have a `name` attribute.
+		The `name_style` attribute, if given, is used to override the default label style.
+		By default, nodes are aligned centrally and are positioned above the node.
+
+		Any additional keyword arguments are considered to be styling options.
+
+		param nodes: The list of nodes for which to draw labels.
+		:type nodes: networkx.classes.reportviews.NodeView
+		:param positions: The positions of the nodes as a dictionary.
+						  The keys are the node names, and the values are the corresponding positions.
+		:type positions: dict
+
+		:return: The list of drawn labels.
+		:rtype: list of :class:`~text.annotation.Annotation`
+		"""
+
+		annotations = [ ]
+
+		"""
+		Extract the node positions and draw the names.
+		"""
+		x = [ position[0] for position in positions.values() ]
+		y = [ position[1] for position in positions.values() ]
+		for node, x, y in zip(nodes, x, y):
+			node = nodes[node]
+			"""
+			Nodes are drawn only if they have a name attribute.
+			"""
+			name = node.get('name')
+			if name:
+				"""
+				By default, node names are aligned centrally and are positioned above the node.
+				However, the style can be overriden by providing a `name_style` attribute.
+				"""
+				default_style = { 'align': 'center', 'va': 'bottom' }
+				default_style.update(**kwargs)
+				style = node.get('name_style', { })
+				default_style.update(style)
+
+				"""
+				The position of the name depends on the node's radius.
+				This is transformed into a padding value.
+
+				.. note::
+
+					The square root of the radius is taken because `the radius is originally squared <https://matplotlib.org/3.2.0/api/_as_gen/matplotlib.pyplot.scatter.html>`_.
+				"""
+				s = node.get('style', { }).get('s', 100)
+				pad = (self.drawable.axis.transData.inverted().transform((0, s ** 0.5))[1] -
+					   self.drawable.axis.transData.inverted().transform((0, 0))[1])
+
+				"""
+				Draw the annotation.
+				"""
+				# TODO: Add support for drawing names on the left or right of nodes.
+				annotation = self.draw_label(name, (x - pad * 2, x + pad * 2), y,
+											 pad=pad/2., **default_style)
+				annotations.append(annotation)
+
+		return annotations
 
 	def _draw_edges(self, edges, nodes, *args, **kwargs):
 		"""
--- multiplex/tests/test_graph.py
+++ multiplex/tests/test_graph.py
@@ -29,7 +29,7 @@ class TestGraph(MultiplexTest):
 		G = nx.Graph()
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		nodes, edges = viz.draw_graph(G)
+		nodes, node_names, edges = viz.draw_graph(G)
 		self.assertFalse(len(nodes))
 		self.assertFalse(edges)
 
@@ -43,7 +43,7 @@ class TestGraph(MultiplexTest):
 		G.add_node(1)
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		nodes, edges = viz.draw_graph(G)
+		nodes, node_names, edges = viz.draw_graph(G)
 		self.assertEqual(1, len(nodes))
 		self.assertFalse(edges)
 
@@ -58,7 +58,7 @@ class TestGraph(MultiplexTest):
 		G.add_node(2)
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		nodes, edges = viz.draw_graph(G)
+		nodes, node_names, edges = viz.draw_graph(G)
 		self.assertEqual(2, len(nodes))
 		self.assertFalse(edges)
 
@@ -75,7 +75,7 @@ class TestGraph(MultiplexTest):
 		G.add_edge(1, 3)
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		nodes, edges = viz.draw_graph(G)
+		nodes, node_names, edges = viz.draw_graph(G)
 		self.assertEqual(3, len(nodes))
 		self.assertEqual(1, len(edges))
 		self.assertEqual(nodes[0].get_offsets()[0][0], edges[0].get_xdata()[0])
@@ -93,7 +93,7 @@ class TestGraph(MultiplexTest):
 		G = nx.from_edgelist(E)
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
-		nodes, edges = viz.draw_graph(G)
+		nodes, node_names, edges = viz.draw_graph(G)
 		self.assertEqual(3, len(nodes))
 		self.assertEqual(2, len(edges))
 		indices = [ (0, 1), (0, 2) ]
@@ -115,7 +115,7 @@ class TestGraph(MultiplexTest):
 
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
 		edge_style = { 'alpha': 0.5, 'color': '#ff0000', 'linewidth': 0.5 }
-		nodes, edges = viz.draw_graph(G, edge_style=edge_style)
+		nodes, node_names, edges = viz.draw_graph(G, edge_style=edge_style)
 		self.assertEqual(3, len(nodes))
 		self.assertEqual(2, len(edges))
 
@@ -135,7 +135,7 @@ class TestGraph(MultiplexTest):
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
 		edge_style = { 'alpha': 0.5 }
 		G.edges[('C', 'A')]['style'] = { 'alpha': 1 }
-		nodes, edges = viz.draw_graph(G, edge_style=edge_style)
+		nodes, node_names, edges = viz.draw_graph(G, edge_style=edge_style)
 		self.assertEqual(3, len(nodes))
 		self.assertEqual(3, len(edges))
 
@@ -155,7 +155,7 @@ class TestGraph(MultiplexTest):
 		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
 		edge_style = { 'alpha': 0.5, 'color': '#FF0000' }
 		G.edges[('C', 'A')]['style'] = { 'alpha': 1 }
-		nodes, edges = viz.draw_graph(G, edge_style=edge_style)
+		nodes, node_names, edges = viz.draw_graph(G, edge_style=edge_style)
 		self.assertEqual(3, len(nodes))
 		self.assertEqual(3, len(edges))
 
@@ -163,3 +163,101 @@ class TestGraph(MultiplexTest):
 		self.assertEqual(0.5, edges[1].get_alpha())
 		self.assertEqual(0.5, edges[2].get_alpha())
 		self.assertTrue(all( edge.get_color() == '#FF0000' for edge in edges ))
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_no_node_names(self):
+		"""
+		Test that when no nodes have names, no names are drawn.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A') ]
+		G = nx.from_edgelist(E)
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, node_names, edges = viz.draw_graph(G)
+		self.assertEqual(3, len(nodes))
+		self.assertEqual(2, len(edges))
+		self.assertFalse(node_names)
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_node_names(self):
+		"""
+		Test that when a node has a name, it is drawn.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A') ]
+		G = nx.from_edgelist(E)
+		G.nodes['A']['name'] = 'A'
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		nodes, node_names, edges = viz.draw_graph(G)
+		self.assertEqual(3, len(nodes))
+		self.assertEqual(2, len(edges))
+		self.assertEqual(1, len(node_names))
+		self.assertEqual('A', str(node_names[0]))
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_node_name_style(self):
+		"""
+		Test that when providing the node name style, it is used when creating annotations.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A') ]
+		G = nx.from_edgelist(E)
+		G.nodes['A']['name'] = 'A'
+		G.nodes['B']['name'] = 'B'
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		name_style = { 'color': '#CC00BB' }
+		nodes, node_names, edges = viz.draw_graph(G, name_style=name_style)
+		self.assertEqual(3, len(nodes))
+		self.assertEqual(2, len(edges))
+		self.assertEqual(2, len(node_names))
+		self.assertTrue(all(name.lines[0][0].get_color() == name_style['color'] for name in node_names))
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_override_node_name_style(self):
+		"""
+		Test that when providing a custom style for an edge, it overwrites the default style.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A') ]
+		G = nx.from_edgelist(E)
+		G.nodes['A']['name'] = 'A'
+		G.nodes['A']['name_style'] = { 'color': '#BBCC00' }
+		G.nodes['B']['name'] = 'B'
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		name_style = { 'color': '#CC00BB' }
+		nodes, node_names, edges = viz.draw_graph(G, name_style=name_style)
+		self.assertEqual(3, len(nodes))
+		self.assertEqual(2, len(edges))
+		self.assertEqual(2, len(node_names))
+		self.assertEqual('#BBCC00', node_names[0].lines[0][0].get_color())
+		self.assertEqual(name_style['color'], node_names[1].lines[0][0].get_color())
+
+	@MultiplexTest.temporary_plot
+	def test_draw_graph_inherit_node_name_style(self):
+		"""
+		Test that when providing a custom style for an edge, it overwrites only the same parameters in the default style.
+		"""
+
+		E = [ ('A', 'C'), ('B', 'A'), ('C', 'B') ]
+		G = nx.from_edgelist(E)
+		G.nodes['A']['name'] = 'A'
+		G.nodes['A']['name_style'] = { 'facecolor': '#3322FF' }
+		G.nodes['B']['name'] = 'B'
+
+		viz = drawable.Drawable(plt.figure(figsize=(10, 5)))
+		name_style = { 'facecolor': '#CC00BB', 'color': '#FFFFFF' }
+		nodes, node_names, edges = viz.draw_graph(G, name_style=name_style)
+		self.assertEqual(3, len(nodes))
+		self.assertEqual(3, len(edges))
+		self.assertEqual(2, len(node_names))
+		self.assertTrue(all(name.lines[0][0].get_color() == name_style['color'] for name in node_names))
+		self.assertEqual(0.2, node_names[0].lines[0][0].get_bbox_patch().get_facecolor()[0])
+		self.assertEqual(round(0.1 + 1/30., 10), round(node_names[0].lines[0][0].get_bbox_patch().get_facecolor()[1], 10))
+		self.assertEqual(1, node_names[0].lines[0][0].get_bbox_patch().get_facecolor()[2])
+		self.assertEqual(0.8, node_names[1].lines[0][0].get_bbox_patch().get_facecolor()[0])
+		self.assertEqual(0, node_names[1].lines[0][0].get_bbox_patch().get_facecolor()[1])
+		self.assertEqual(round(0.7 + 1/30., 10), round(node_names[1].lines[0][0].get_bbox_patch().get_facecolor()[2], 10))
