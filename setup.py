df8012a0349752f7096979c545d3c70b78577263
NicholasMamo <nicholasmamo@gmail.com>
2020-05-22 15:39:24 +0200

    Create version 0.3.4 of the library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multiplex-plot", # Replace with your own username
-    version="0.2.7",
+    version="0.3.4",
     author="Nicholas Mamo",
     author_email="nicholasmamo@gmail.com",
     description="Multiplex: visualizations that tell stories",
