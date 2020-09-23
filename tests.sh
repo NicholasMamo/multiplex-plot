186d47964e4165ea5ec9fa832142682e6436f40f
NicholasMamo <nicholasmamo@gmail.com>
2020-09-23 15:14:23 +0200

    Add the util test class to the general test script

--- tests.sh
+++ tests.sh
@@ -14,3 +14,4 @@ python3 -m unittest multiplex.tests.test_labelled
 python3 -m unittest multiplex.tests.test_legend
 python3 -m unittest multiplex.text.tests.test_text
 python3 -m unittest multiplex.timeseries.tests.test_time_series
+python3 -m unittest multiplex.tests.test_util
