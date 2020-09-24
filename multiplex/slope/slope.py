535cfe339bc193b9160506e1d805bf2ab900687b
NicholasMamo <nicholasmamo@gmail.com>
2020-09-24 14:44:36 +0200

    Add the slope graph styling

--- multiplex/slope/slope.py
+++ multiplex/slope/slope.py
@@ -34,14 +34,52 @@ class Slope(LabelledVisualization):
     Like all visualizations, it revolves around the :func:`~Slope.draw` function.
     """
 
-    def draw(self, *args, **kargs):
+    def draw(self, style_plot=True, *args, **kargs):
         """
         Draw a slope graph.
         The function returns a two-tuple with the drawn plot (a line with optional markers) and any drawn labels.
+
+        :param style_plot: A boolean indicating whether the plot should be re-styled.
+                           If it is set to ``True``, the visualization:
+
+                           - Removes the grid,
+                           - Hides the x-axis
+                           - Hides the y-axis, and
+                           - Adds two x-ticks.
+        :type style_plot: bool
 
         :return: A tuple made up of the drawn plot and any drawn labels.
                  If the legend label is drawn, only a string is returned.
         :rtype: tuple
         """
 
+        """
+        Re-style the plot if need be.
+        """
+        if style_plot:
+            self._style()
+
         return (None, None)
+
+    def _style(self):
+        """
+        Style the plot by:
+
+        - Removes the grid,
+        - Hides the x-axis
+        - Hides the y-axis, and
+        - Adds two x-ticks.
+        """
+
+        axes = self.drawable.axes
+        
+        self.drawable.grid(False)
+        axes.spines['top'].set_visible(False)
+        axes.spines['right'].set_visible(False)
+        axes.spines['bottom'].set_visible(False)
+        axes.spines['left'].set_visible(False)
+        self.drawable.set_xlim((-0.5, 1))
+        self.drawable.set_xticks(range(0, 2))
+        self.drawable.set_yticks([ ])
+        axes.xaxis.set_label_position('bottom')
+        axes.xaxis.tick_bottom()
--- multiplex/slope/tests/test_slope.py
+++ multiplex/slope/tests/test_slope.py
@@ -28,3 +28,45 @@ class TestSlope(MultiplexTest):
 
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertEqual(tuple, type(viz.draw_slope()))
+
+    @MultiplexTest.temporary_plot
+    def test_draw_with_style_plot(self):
+        """
+        Test that when setting the style, the following changes are made:
+
+        - Removes the grid,
+        - Hides the x-axis
+        - Hides the y-axis, and
+        - Adds two x-ticks.
+        """
+
+        viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
+        viz.draw_slope(style_plot=True)
+        self.assertFalse(viz.axes.spines['top'].get_visible())
+        self.assertFalse(viz.axes.spines['right'].get_visible())
+        self.assertFalse(viz.axes.spines['bottom'].get_visible())
+        self.assertFalse(viz.axes.spines['left'].get_visible())
+        self.assertEqual((-0.5, 1), viz.get_xlim())
+        self.assertEqual([ 0, 1 ], list(viz.get_xticks()))
+        self.assertEqual([ ], list(viz.get_yticks()))
+
+    @MultiplexTest.temporary_plot
+    def test_draw_default_style_plot(self):
+        """
+        Test that when not setting the style, none of the the following changes are made:
+
+        - Removes the grid,
+        - Hides the x-axis
+        - Hides the y-axis, and
+        - Adds two x-ticks.
+        """
+
+        viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
+        viz.draw_slope(style_plot=False)
+        self.assertTrue(viz.axes.spines['top'].get_visible())
+        self.assertTrue(viz.axes.spines['right'].get_visible())
+        self.assertTrue(viz.axes.spines['bottom'].get_visible())
+        self.assertTrue(viz.axes.spines['left'].get_visible())
+        self.assertEqual((0, 1), viz.get_xlim())
+        self.assertEqual([ 0, 1/5, 2/5, 3/5, 4/5, 1 ], [ round(tick, 1) for tick in viz.get_xticks() ])
+        self.assertEqual([ 0, 1/5, 2/5, 3/5, 4/5, 1 ], [ round(tick, 1) for tick in viz.get_yticks() ])
