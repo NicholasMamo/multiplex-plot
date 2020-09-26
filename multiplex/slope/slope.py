6f2b342b90e9a2bf6822ac1299e9bf55c57152c9
NicholasMamo <nicholasmamo@gmail.com>
2020-09-26 15:42:32 +0200

    Udpate the styling of the slope graph

--- multiplex/slope/slope.py
+++ multiplex/slope/slope.py
@@ -71,14 +71,17 @@ class Slope(LabelledVisualization):
         - Adds two x-ticks.
         """
 
-        axes = self.drawable.axes
+        # TODO: Add the secondary axes
         
+        axes = self.drawable.axes
+
         self.drawable.grid(False)
         axes.spines['top'].set_visible(False)
         axes.spines['right'].set_visible(False)
-        axes.spines['bottom'].set_visible(False)
+        axes.spines['bottom'].set_visible(True)
+        axes.spines['bottom'].set_bounds(0, 1)
         axes.spines['left'].set_visible(False)
-        self.drawable.set_xlim((-0.5, 1))
+        self.drawable.set_xlim((-0.5, 1.2))
         self.drawable.set_xticks(range(0, 2))
         self.drawable.set_yticks([ ])
         axes.xaxis.set_label_position('bottom')
--- multiplex/slope/tests/test_slope.py
+++ multiplex/slope/tests/test_slope.py
@@ -27,7 +27,7 @@ class TestSlope(MultiplexTest):
         """
 
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
-        self.assertEqual(tuple, type(viz.draw_slope()))
+        self.assertEqual(tuple, type(viz.draw_slope(5, 5, )))
 
     @MultiplexTest.temporary_plot
     def test_draw_with_style_plot(self):
@@ -41,12 +41,13 @@ class TestSlope(MultiplexTest):
         """
 
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
-        viz.draw_slope(style_plot=True)
+        viz.draw_slope(5, 5, style_plot=True)
         self.assertFalse(viz.axes.spines['top'].get_visible())
         self.assertFalse(viz.axes.spines['right'].get_visible())
-        self.assertFalse(viz.axes.spines['bottom'].get_visible())
+        self.assertTrue(viz.axes.spines['bottom'].get_visible())
+        self.assertEqual((0, 1), viz.axes.spines['bottom'].get_bounds())
         self.assertFalse(viz.axes.spines['left'].get_visible())
-        self.assertEqual((-0.5, 1), viz.get_xlim())
+        self.assertEqual((-0.5, 1.2), viz.get_xlim())
         self.assertEqual([ 0, 1 ], list(viz.get_xticks()))
         self.assertEqual([ ], list(viz.get_yticks()))
 
@@ -62,11 +63,11 @@ class TestSlope(MultiplexTest):
         """
 
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
-        viz.draw_slope(style_plot=False)
+        viz.draw_slope(0, 0, style_plot=False)
         self.assertTrue(viz.axes.spines['top'].get_visible())
         self.assertTrue(viz.axes.spines['right'].get_visible())
         self.assertTrue(viz.axes.spines['bottom'].get_visible())
         self.assertTrue(viz.axes.spines['left'].get_visible())
-        self.assertEqual((0, 1), viz.get_xlim())
-        self.assertEqual([ 0, 1/5, 2/5, 3/5, 4/5, 1 ], [ round(tick, 1) for tick in viz.get_xticks() ])
-        self.assertEqual([ 0, 1/5, 2/5, 3/5, 4/5, 1 ], [ round(tick, 1) for tick in viz.get_yticks() ])
+        self.assertEqual((-0.05, 1.05), viz.get_xlim())
+        self.assertEqual([ -1/5, 0, 1/5, 2/5, 3/5, 4/5, 1, 6/5 ], [ round(tick, 2) for tick in viz.get_xticks() ])
+        self.assertEqual([ -6/100, -4/100, -2/100, 0, 2/100, 4/100, 6/100 ], [ round(tick, 2) for tick in viz.get_yticks() ])
