1f31f9720c1a066335ac1b3a8846f67d0c302f38
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-31 18:41:27 +0100

    Fix the population unit tests not discarding plots

--- multiplex/population/tests/test_population.py
+++ multiplex/population/tests/test_population.py
@@ -21,6 +21,7 @@ class TestPopulation(MultiplexTest):
     Unit tests for the :class:`~population.population.Population` class.
     """
 
+    @MultiplexTest.temporary_plot
     def test_draw_float_rows(self):
         """
         Test that when drawing with a floating point number of rows, the function raises a TypeError.
@@ -29,6 +30,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(TypeError, viz.draw_population, 5, 1.2)
 
+    @MultiplexTest.temporary_plot
     def test_draw_negative_rows(self):
         """
         Test that when drawing with a negative number of rows, the function raises a ValueError.
@@ -37,6 +39,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, 5, -1)
 
+    @MultiplexTest.temporary_plot
     def test_draw_zero_rows(self):
         """
         Test that when drawing with no rows, the function raises a ValueError.
@@ -45,6 +48,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, 5, 0)
 
+    @MultiplexTest.temporary_plot
     def test_draw_float_population(self):
         """
         Test that when drawing with a floating point population, the function raises a TypeError.
@@ -53,6 +57,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(TypeError, viz.draw_population, 2.4, 10)
 
+    @MultiplexTest.temporary_plot
     def test_draw_negative_population(self):
         """
         Test that when drawing with a negative population, the function raises a ValueError.
@@ -61,6 +66,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, -5, 10)
 
+    @MultiplexTest.temporary_plot
     def test_draw_negative_height(self):
         """
         Test that when drawing with a negative population height, the function raises a ValueError.
@@ -69,6 +75,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, 5, 10, height=-1)
 
+    @MultiplexTest.temporary_plot
     def test_draw_zero_height(self):
         """
         Test that when drawing with a zero population height, the function raises a ValueError.
@@ -77,6 +84,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, 5, 10, height=0)
 
+    @MultiplexTest.temporary_plot
     def test_draw_height_one(self):
         """
         Test that when drawing with a population height of 1, the function accepts it.
@@ -85,6 +93,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertTrue(viz.draw_population(5, 10, height=1))
 
+    @MultiplexTest.temporary_plot
     def test_draw_large_height(self):
         """
         Test that when drawing with a large population height, the function raises a ValueError.
@@ -93,6 +102,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertRaises(ValueError, viz.draw_population, 5, 10, height=2)
 
+    @MultiplexTest.temporary_plot
     def test_draw_zero_population(self):
         """
         Test that when drawing an empty population, the function returns an empty list of points.
@@ -101,6 +111,7 @@ class TestPopulation(MultiplexTest):
         viz = drawable.Drawable(plt.figure(figsize=(10, 10)))
         self.assertEqual([ ], viz.draw_population(0, 10))
 
+    @MultiplexTest.temporary_plot
     def test_draw_correct_rows_square(self):
         """
         Test that when drawing a population, the number of rows in each column (except incomplete columns) is equal to the given number of rows.
@@ -111,6 +122,7 @@ class TestPopulation(MultiplexTest):
         drawn = viz.draw_population(30, rows)
         self.assertTrue(all( rows == len(column) for column in drawn ))
 
+    @MultiplexTest.temporary_plot
     def test_draw_correct_rows_uneven(self):
         """
         Test that when drawing a population, the number of rows in each column is equal to the given number of rows, except for the last column when the population is not a factor of the rows.
@@ -122,6 +134,7 @@ class TestPopulation(MultiplexTest):
         self.assertTrue(all( rows == len(column) for column in drawn[:-1] ))
         self.assertTrue(all( len(drawn[-1]) < len(column) for column in drawn[:-1] ))
 
+    @MultiplexTest.temporary_plot
     def test_draw_equal_population(self):
         """
         Test that when drawing a population, all points are drawn.
@@ -132,6 +145,7 @@ class TestPopulation(MultiplexTest):
         drawn = viz.draw_population(population, rows)
         self.assertEqual(population, sum( len(column) for column in drawn ))
 
+    @MultiplexTest.temporary_plot
     def test_draw_equal_population_uneven_columns(self):
         """
         Test that when drawing a population, the correct number of points are drawn even when the last column is incomplete.
@@ -142,6 +156,7 @@ class TestPopulation(MultiplexTest):
         drawn = viz.draw_population(population, rows)
         self.assertEqual(population, sum( len(column) for column in drawn ))
 
+    @MultiplexTest.temporary_plot
     def test_draw_do_not_overlap(self):
         """
         Test that none of the drawn points in a population overlap.
@@ -154,6 +169,7 @@ class TestPopulation(MultiplexTest):
             for j in range(i + 1, len(points)):
                 self.assertFalse(util.overlapping(viz.figure, viz.axes, points[i], points[j]))
 
+    @MultiplexTest.temporary_plot
     def test_draw_fits_within_height(self):
         """
         Test that the points fit within the given height.
@@ -180,6 +196,7 @@ class TestPopulation(MultiplexTest):
             bb = util.get_bb(viz.figure, viz.axes, column[-1])
             self.assertEqual(lim[1], (bb.y0 + bb.y1) / 2)
 
+    @MultiplexTest.temporary_plot
     def test_draw_rows_align(self):
         """
         Test that the drawn points align along the same row.
@@ -202,6 +219,7 @@ class TestPopulation(MultiplexTest):
                 self.assertEqual(bb.y1, _bb.y1)
                 self.assertEqual(bb.height, _bb.height)
 
+    @MultiplexTest.temporary_plot
     def test_draw_rows_equidistant(self):
         """
         Test that the rows are separated with the same gap.
@@ -225,6 +243,7 @@ class TestPopulation(MultiplexTest):
             bb_next = util.get_bb(viz.figure, viz.axes, rows[1][0])
             self.assertEqual(gap, bb.y1 - bb_next.y0)
 
+    @MultiplexTest.temporary_plot
     def test_draw_columns_align(self):
         """
         Test that the drawn points align along the same column.
@@ -243,6 +262,7 @@ class TestPopulation(MultiplexTest):
                 self.assertEqual(bb.x1, _bb.x1)
                 self.assertEqual(bb.width, _bb.width)
 
+    @MultiplexTest.temporary_plot
     def test_draw_columns_equidistant(self):
         """
         Test that the columns are separated with the same gap.
@@ -262,6 +282,7 @@ class TestPopulation(MultiplexTest):
             bb_next = util.get_bb(viz.figure, viz.axes, drawn[1][0])
             self.assertEqual(gap, bb.x1 - bb_next.x0)
 
+    @MultiplexTest.temporary_plot
     def test_draw_apply_general_style(self):
         """
         Test that when passing on keyword arguments, they are treated as the general style and applied to all points.
@@ -273,6 +294,7 @@ class TestPopulation(MultiplexTest):
         points = [ point for column in drawn for point in column ]
         self.assertTrue(all( [241/255, 66/255, 138/255, 1] == point.get_facecolor().tolist()[0] for point in points ))
 
+    @MultiplexTest.temporary_plot
     def test_limit_negative_height(self):
         """
         Test that when drawing with a negative population height, the function raises a ValueError.
@@ -281,6 +303,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(ValueError, viz._limit, -1)
 
+    @MultiplexTest.temporary_plot
     def test_limit_zero_height(self):
         """
         Test that when drawing with a zero population height, the function raises a ValueError.
@@ -289,6 +312,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(ValueError, viz._limit, 0)
 
+    @MultiplexTest.temporary_plot
     def test_limit_height_one(self):
         """
         Test that when drawing with a population height of 1, the function accepts it.
@@ -297,6 +321,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertTrue(viz._limit(1))
 
+    @MultiplexTest.temporary_plot
     def test_limit_large_height(self):
         """
         Test that when drawing with a large population height, the function raises a ValueError.
@@ -305,6 +330,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(ValueError, viz._limit, 2)
 
+    @MultiplexTest.temporary_plot
     def test_limit(self):
         """
         Test calculating the limit.
@@ -313,6 +339,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertEqual((0.25, 0.75), viz._limit(0.5))
 
+    @MultiplexTest.temporary_plot
     def test_limit_order(self):
         """
         Test that the limit is a tuple in ascending order.
@@ -323,6 +350,7 @@ class TestPopulation(MultiplexTest):
         self.assertEqual(tuple, type(limit))
         self.assertLess(limit[0], limit[1])
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_float_rows(self):
         """
         Test that when getting the gap size and the number of rows is a float, the function raises a TypeError.
@@ -331,6 +359,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(TypeError, viz._gap_size, (0, 1), 1.2)
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_negative_rows(self):
         """
         Test that when getting the gap size and the number of rows is a negative integer, the function raises a ValueError.
@@ -339,6 +368,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(ValueError, viz._gap_size, (0, 1), -1)
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_zero_rows(self):
         """
         Test that when getting the gap size and the number of rows is zero, the function raises a ValueError.
@@ -347,6 +377,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertRaises(ValueError, viz._gap_size, (0, 1), 0)
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_one_row(self):
         """
         Test that that the gap size of one row is 0.
@@ -355,6 +386,7 @@ class TestPopulation(MultiplexTest):
         viz = Population(drawable.Drawable)
         self.assertEqual(0, viz._gap_size((0, 1), 1))
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_two_rows(self):
         """
         Test that that the gap size of two rows is equivalent to the gap between the limits.
@@ -368,6 +400,7 @@ class TestPopulation(MultiplexTest):
         lim = (0.2, 0.8)
         self.assertEqual(lim[1] - lim[0], viz._gap_size(lim, 2))
 
+    @MultiplexTest.temporary_plot
     def test_gap_size_multiple_rows(self):
         """
         Test that that the gap size of multiple rows fills the space between the limits.
