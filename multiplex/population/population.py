35ff993514b824c3394a15510f9e7f30e1d06a56
Nicholas Mamo <nicholasmamo@gmail.com>
2021-01-31 14:08:16 +0100

    Create the initial population class

---
+++ multiplex/population/population.py
@@ -0,0 +1,15 @@
+"""
+Multiplex's population chart is a brand new type of visualization that builds on matplotlib.
+This visualization makes it easy to show how different populations, in a broad sense, vary from each other.
+"""
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+import util
+
+from visualization import Visualization
+
+class Population(Visualization):
+    """
+    The :class:`~Population` class plots scatter points that represent populations.
+    Like all visualizations, it stores a :class:`~drawable.Drawable` instance and revolves around the :func:`~Population.draw` function.
+    """
